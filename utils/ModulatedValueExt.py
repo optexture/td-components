be60eb199600863e0b44a8f09fe51934438410bc
tekt <tekt@optexture.com>
2019-11-07 23:29:36 -0800

    modulated value helper

--- utils/ModulatedValueExt.py
+++ utils/ModulatedValueExt.py
@@ -7,16 +7,3 @@ class ModulatedValue:
 	def __init__(self, ownerComp):
 		self.ownerComp = ownerComp
 
-	# def BuildValueSettings(self, dat: 'DAT'):
-	# 	dat.clear()
-	# 	dat.appendRow(['index', 'name', 'valuePar', 'modValuePar'])
-	# 	for i in range(1, 5):
-	# 		name = getattr(self.ownerComp.par, 'Valuename{}'.format(i))
-	# 		if not name:
-	# 			continue
-	# 		dat.appendRow([
-	# 			i,
-	# 			name,
-	# 			'Value{}'.format(i),
-	# 			'Modvalue{}'.format(i),
-	# 		])
