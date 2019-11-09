5dcc73858691ebfe0ee7199f27d9bf303eb36463
tekt <tekt@optexture.com>
2019-11-08 20:22:05 -0800

    modulated value things

--- utils/ModulatedValueExt.py
+++
@@ -1,9 +0,0 @@
-# noinspection PyUnreachableCode
-if False:
-	# noinspection PyUnresolvedReferences
-	from _stubs import *
-
-class ModulatedValue:
-	def __init__(self, ownerComp):
-		self.ownerComp = ownerComp
-
