92377a12142830e9dbcca256dccc194f35dbf247
tekt <tekt@optexture.com>
2020-05-28 20:55:38 -0700

    starting on some editor things

---
+++ rack/editor/EditorExt.py
@@ -0,0 +1,11 @@
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+	from typing import Any
+	iop.hostedComp = COMP()
+	ipar.compEditor = Any()
+
+class Editor:
+	def __init__(self, ownerComp):
+		self.ownerComp = ownerComp
