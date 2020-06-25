6e150795de2428d6779db4f850160c195fe27e66
tekt <tekt@optexture.com>
2020-06-24 21:01:51 -0700

    starting a settings system

---
+++ rack/editor/components/SettingsExt.py
@@ -0,0 +1,45 @@
+"""
+Extension classes enhance TouchDesigner components with python. An
+extension is accessed via ext.ExtensionClassName from any operator
+within the extended component. If the extension is promoted via its
+Promote Extension parameter, all its attributes with capitalized names
+can be accessed externally, e.g. op('yourComp').PromotedFunction().
+
+Help: search "Extensions" in wiki
+"""
+
+from TDStoreTools import StorageManager
+TDF = op.TDModules.mod.TDFunctions
+
+class SettingsExt:
+	"""
+	SettingsExt description
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+
+		# properties
+		TDF.createProperty(self, 'MyProperty', value=0, dependable=True,
+						   readOnly=False)
+
+		# attributes:
+		self.a = 0 # attribute
+		self.B = 1 # promoted attribute
+
+		# stored items (persistent across saves and re-initialization):
+		storedItems = [
+			# Only 'name' is required...
+			{'name': 'StoredProperty', 'default': None, 'readOnly': False,
+			 						'property': True, 'dependable': True},
+		]
+		# Uncomment the line below to store StoredProperty. To clear stored
+		# 	items, use the Storage section of the Component Editor
+		
+		# self.stored = StorageManager(self, ownerComp, storedItems)
+
+	def myFunction(self, v):
+		debug(v)
+
+	def PromotedFunction(self, v):
+		debug(v)
