9e31d53b382aee433720c63da02f09793b3322e5
tekt <tekt@optexture.com>
2020-07-01 17:37:12 -0700

    minor cleanup in param filter

--- utils/ParamFilter.py
+++ utils/ParamFilter.py
@@ -3,38 +3,34 @@ if False:
 	# noinspection PyUnresolvedReferences
 	from _stubs import *
 
-# + prefix marks the params 'advanced'
-_prefix = '+'
-
 def InitHost():
-	filter = parent()
-	hostop = filter.par.Parfilterop.eval()
-	if not hostop:
+	filterOp = parent()
+	hostOp = filterOp.par.Parfilterop.eval()
+	if not hostOp:
 		return
-	prefix = filter.par.Installedlabelprefix.eval()
-	page = hostop.appendCustomPage('Param Filter')
-	fp = filter.par.Parfilterenable
+	prefix = filterOp.par.Installedlabelprefix.eval()
+	page = hostOp.appendCustomPage('Param Filter')
+	fp = filterOp.par.Parfilterenable
 	page.appendToggle(fp.name, label=prefix + fp.label)
-	fp = filter.par.Parfiltertype
+	fp = filterOp.par.Parfiltertype
 	p = page.appendMenu(fp.name, label=prefix + fp.label)[0]
 	p.menuNames = fp.menuNames
 	p.menuLabels = fp.menuLabels
 	p.default = fp.default
-	_copyFloatPar(page, filter.par.Parfilterwidth, prefix)
-	_copyFloatPar(page, filter.par.Parfilterlag1, prefix)
-	_copyFloatPar(page, filter.par.Parfilterovershoot1, prefix)
+	_copyFloatPar(page, filterOp.par.Parfilterwidth, prefix)
+	_copyFloatPar(page, filterOp.par.Parfilterlag1, prefix)
+	_copyFloatPar(page, filterOp.par.Parfilterovershoot1, prefix)
 
-	relpath = filter.relativePath(hostop)
-	exprprefix = 'op({!r}).par.'.format(relpath)
-	for p in filter.customPages[0].pars:
-		p.expr = exprprefix + p.name
+	exprPrefix = 'op("{}").par.'.format(filterOp.relativePath(hostOp))
+	for p in filterOp.customPages[0].pars:
+		p.expr = exprPrefix + p.name
 
-def _copyFloatPar(page, src, prefix):
-	srctup = src.tuplet
-	ptup = page.appendFloat(src.tupletName, label=prefix + src.label, size=len(srctup))
-	for i, srcp in enumerate(srctup):
+def _copyFloatPar(page: 'Page', src: 'Par', prefix: str):
+	sourceTuplet = src.tuplet
+	parTuplet = page.appendFloat(src.tupletName, label=prefix + src.label, size=len(sourceTuplet))
+	for i, srcp in enumerate(sourceTuplet):
 		for a in (
 				'default', 'normMin', 'normMax',
 				'clampMin', 'clampMax', 'min', 'max'
 		):
-			setattr(ptup[i], a, getattr(srcp, a))
+			setattr(parTuplet[i], a, getattr(srcp, a))
